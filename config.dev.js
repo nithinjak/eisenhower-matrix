ef5b404f1eba60229df64d08889715ded0b8d2d2
Nithin Jakamputi <nithinjakamputi@Nithins-MacBook-Air.local>
2026-05-06 17:15:40 +0530

    config: separate config files for dev and prod

---
+++ config.dev.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL      = "https://cbewikyrbxyvlatrxtmp.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiZXdpa3lyYnh5dmxhdHJ4dG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTE3MTgsImV4cCI6MjA5MzYyNzcxOH0.os7-_4AYDr_F75LMkmUN0FPfKC--rpOHLBNzzUtwPHI";
